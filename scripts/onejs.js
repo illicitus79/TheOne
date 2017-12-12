cebf61295752bc1bf9c3d242af26cae8247c9919
illicitus79 <illicitus79@gmail.com>
2017-12-12 11:32:21 +0700

    Full apps configured with home icon

--- scripts/onejs.js
+++ scripts/onejs.js
@@ -41,3 +41,89 @@ function calculatecd(){
 	    // ?alert("done");
 	}
 }
+
+function createbarcode()
+{
+	var lines = document.getElementById("itminput").value.split("\n");
+	if(lines[0]=="")
+	{
+		alert("Please enter the Item Ids for generating barcodes");
+	}
+	else
+	{
+		for(var i = 0;i < lines.length;)
+		{
+			if(lines[i]!="")
+			{
+				if(i!=lines.length-1)
+				{
+					document.getElementById("tblBarcde").innerHTML+="<tr><td><svg id=\"barcode"+i+"\"></svg></td><td><svg id=\"barcode"+(i+1)+"\"></svg></td></tr>";
+					JsBarcode("#barcode"+i, lines[i]);
+					JsBarcode("#barcode"+(i+1), lines[i+1]);
+					i=i+2;
+				}
+				else
+				{
+					document.getElementById("tblBarcde").innerHTML+="<tr><td><svg id=\"barcode"+i+"\"></svg></td><td></td></tr>";
+					JsBarcode("#barcode"+i, lines[i]);
+					i++;
+				}
+			}
+			else
+			{
+				i++;
+			}
+		}
+		document.getElementById("printbtn").style.display="block";
+		document.getElementById("usrin").style.display="none";
+		document.getElementById("tblBarcde").style.display="block";
+	}
+}
+
+
+function printdiv(){
+     var printContents = document.getElementById("barcgen").innerHTML;
+     var originalContents = document.body.innerHTML;
+     document.body.innerHTML = printContents;
+     window.print();
+     document.body.innerHTML = originalContents;
+}
+
+
+function showcalccd(){
+	document.getElementById("calccheckd").style.display="block";
+	document.getElementById("imgslde").style.display="none";
+	document.getElementById("barcgen").style.display="none";
+}
+
+
+function showbargen(){
+	document.getElementById("imgslde").style.display="none";
+	document.getElementById("barcgen").style.display="block";
+	document.getElementById("calccheckd").style.display="none";
+	document.getElementById("usrin").style.display="block";
+	document.getElementById("tblBarcde").style.display="none";
+	document.getElementById("printbtn").style.display="none";
+	document.getElementById("tblBarcde").innerHTML="";
+
+}
+
+
+
+
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";  
+	}
+	myIndex++;
+	if (myIndex > x.length) {myIndex = 1}    
+		x[myIndex-1].style.display = "block";  
+	setTimeout(carousel, 9000);    
+}
